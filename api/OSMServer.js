8b6fe09a609a015ad2d2356dd59e291863c395a4
James Morgan <james.morgan.e@googlemail.com>
2018-12-04 14:00:47 +0000

    Adding supertest

--- api/OSMServer.js
+++ api/OSMServer.js
@@ -4,16 +4,14 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-// Config
-const config = require('config');
-const apiConfig = config.get('api');
-
 const app = express();
 app.use(cors());
 app.use(express.json({strict: true}));
 app.use(bodyParser.json());
 
 // TODO add authentication
+// TODO add JSON Schema validator once API more defined
+// TODO add unhandled route middleware
 
 const keys = require('./routes/keys');
 const messages = require('./routes/messages');
@@ -21,4 +19,4 @@ const messages = require('./routes/messages');
 app.use('/keys', keys);
 app.use('/messages', messages);
 
-app.listen(apiConfig.port, () => console.log(`listening on ${apiConfig.port}`));
+module.exports = app;
