31c9becae553aeadb5b4be29ceafbc767c90409b
James Morgan <james.morgan.e@googlemail.com>
2018-12-04 11:53:49 +0000

    Adding basic hello world test for get/put keys

---
+++ test/api/services/KeyService.spec.js
@@ -0,0 +1,30 @@
+const expect = require('chai').expect;
+
+const KeyService = require('../../../api/services/KeyService');
+
+// DB should use test-db due to config
+const db = require('../../../api/database');
+
+describe('KeyService tests', function () {
+
+    beforeEach(() => {
+        this.keyService = new KeyService(db);
+    });
+
+    it('should be able to put & get the same key', async () => {
+        const deviceId = "123";
+        const registrationId = "ABC";
+        const someOtherData = {test: [1], bool: true, sting: "A string"};
+
+        const key = {
+            deviceId,
+            registrationId,
+            someOtherData
+        };
+        await this.keyService.put(key);
+
+        const found = await this.keyService.get(deviceId, registrationId);
+
+        expect(found).to.be.deep.equal(key);
+    });
+});
